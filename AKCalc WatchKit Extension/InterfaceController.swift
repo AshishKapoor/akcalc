8ef5f9184e59fd617c442b666f13dd442d6a2a97
Ashish Kapoor <swiftobjc@gmail.com>
2018-07-18 13:36:02 +0530

    connected all the outlets.

--- AKCalc WatchKit Extension/InterfaceController.swift
+++ AKCalc WatchKit Extension/InterfaceController.swift
@@ -12,6 +12,38 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var valueLabel: WKInterfaceLabel!
+    @IBAction func tappedZero(){tappedNumber(num: 0)}
+    @IBAction func tappedOne(){tappedNumber(num: 1)}
+    @IBAction func tappedTwo(){tappedNumber(num: 2)}
+    @IBAction func tappedThree(){tappedNumber(num: 3)}
+    @IBAction func tappedFour(){tappedNumber(num: 4)}
+    @IBAction func tappedFive(){tappedNumber(num: 5)}
+    @IBAction func tappedSix(){tappedNumber(num: 6)}
+    @IBAction func tappedSeven(){tappedNumber(num: 7)}
+    @IBAction func tappedEight(){tappedNumber(num: 8)}
+    @IBAction func tappedNine(){tappedNumber(num: 9)}
+    
+    @IBAction func tappedPlus() {
+        print("Tapped Plus");
+    }
+    
+    @IBAction func tappedMinus() {
+        print("Tapped Minus");
+    }
+    
+    @IBAction func tappedClear() {
+        print("Tapped Clear");
+    }
+    
+    @IBAction func tappedEquals() {
+        print("Tapped Equals");
+    }
+    
+    func tappedNumber(num: Int) {
+        print("Tapped: \(num)");
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
